8aea77211a18c19cebe9e40bd3d1df804a76fcb6
Glad NZAOU <114177610+Glad-Nagil@users.noreply.github.com>
2024-01-28 16:03:05 +0100

    Adding style for default Text

--- src/theme/style.ts
+++ src/theme/style.ts
@@ -1,216 +1,27 @@
-// Définir le type des objets de styles
-interface LabelStyles {
+const defaultLabelStyles = {
   FormLabel: {
-    baseStyleRegular: {
-      fontFamily: {
-        regular: string;
-      };
-      color: string;
-      fontSize: string;
-      fontStyle: string;
-      fontWeight: string;
-      lineHeight: string;
-    };
-    baseStyleRegular2: {
-      fontFamily: {
-        regular: string;
-      };
-      color: string;
-      fontSize: string;
-      fontStyle: string;
-      fontWeight: string;
-      lineHeight: string;
-    };
-    baseStyleMedium: {
-      fontFamily: {
-        medium: string;
-      };
-      color: string;
-      fontSize: string;
-      fontStyle: string;
-      fontWeight: string;
-      lineHeight: string;
-    };
-    baseStyleBold: {
-      fontFamily: {
-        bold: string;
-      };
-      color: string;
-      fontSize: string;
-      fontStyle: string;
-      fontWeight: string;
-      lineHeight: string;
-    };
-    baseStyleBold2: {
-      fontFamily: {
-        bold: string;
-      };
-      color: string;
-      fontSize: string;
-      fontStyle: string;
-      fontWeight: string;
-      lineHeight: string;
-    };
-    baseStyleBold3: {
-      fontFamily: {
-        bold: string;
-      };
-      color: string;
-      fontSize: string;
-      fontStyle: string;
-      fontWeight: string;
-      lineHeight: string;
-    };
-    loginStyle: {
-      fontFamily: {
-        regular: string;
-      };
-      color: string;
-      fontSize: string;
-      fontStyle: string;
-      fontWeight: string;
-      lineHeight: string;
-      width: string;
-      height: string;
-      flexShrink: string;
-    };
-    loginStyle2: {
-      fontFamily: {
-        regular: string;
-      };
-      color: string;
-      fontSize: string;
-      fontStyle: string;
-      fontWeight: string;
-      lineHeight: string;
-      width: string;
-      height: string;
-      flexShrink: string;
-    };
-    passwordLogin: {
-      fontFamily: {
-        regular: string;
-      };
-      color: string;
-      fontSize: string;
-      fontStyle: string;
-      fontWeight: string;
-      lineHeight: string;
-      width: string;
-      height: string;
-      flexShrink: string;
-      textDecoration: string;
-    };
-  };
-}
-
-// Utilisez le type pour l'objet de styles
-const defaultLabelStyles: LabelStyles = {
-  FormLabel: {
-    baseStyleRegular: {
-      fontFamily: {
-        regular: "Lato, sans-serif",
-      },
-      color: "#000",    
-      fontSize: "15px",
-      fontStyle: "normal",
-      fontWeight: "400",
-      lineHeight: "normal",
-    },
-    baseStyleRegular2: {
-      fontFamily: {
-        regular: "Lato, sans-serif",
-      },
-      color: "#000",    
-      fontSize: "12px",
-      fontStyle: "normal",
-      fontWeight: "500",
-      lineHeight: "normal",
-    },
-    baseStyleMedium: {
-      fontFamily: {
-        medium: "Lato Medium, sans-serif",
-      },
-      color: "#000",    
-      fontSize: "14px",
-      fontStyle: "normal",
-      fontWeight: "500",
-      lineHeight: "normal",
-    },
-    baseStyleBold: {
-      fontFamily: {
-       
-        bold: "Lato",
-      },
-      color: "#050505",    
-      fontSize: "20px",
-      fontStyle: "normal",
-      fontWeight: "700",
-      lineHeight: "normal",
-    },
-    baseStyleBold2: {
-      fontFamily: {
-       
-        bold: "Lato",
-      },
-      color: "#050505",    
-      fontSize: "15px",
-      fontStyle: "normal",
-      fontWeight: "700",
-      lineHeight: "normal",
-    },
-    baseStyleBold3: {
-      fontFamily: {
-       
-        bold: "Lato",
-      },
-      color: "#050505",    
-      fontSize: "17px",
-      fontStyle: "normal",
-      fontWeight: "700",
-      lineHeight: "normal",
-    },
-    loginStyle: {
-      fontFamily: {
-        regular: "Afterglow",
-      },
+    baseStyle: {
+      fontFamily: "lato-medium",
       color: "secondary.500",
-      fontSize: "50px",
-      fontStyle: "normal",
-      fontWeight: "400",
-      lineHeight: "normal",
-      width: "135px",
-      height: "47px",
-      flexShrink: "0",
-    },
-    loginStyle2: {
-      fontFamily: {
-        regular: "Afterglow",
-      },
-      color: "secondary.500",
-      fontSize: "13px",
-      fontStyle: "normal",
-      fontWeight: "400",
-      lineHeight: "normal",
-      width: "135px",
-      height: "47px",
-      flexShrink: "0",
-    },
-    passwordLogin: {
-      fontFamily: {
-        regular: "Afterglow",
-      },
-      color: "secondary.500",
-      fontSize: "13px",
-      fontStyle: "normal",
-      fontWeight: "400",
-      lineHeight: "normal",
-      width: "135px",
-      height: "47px",
-      flexShrink: "0",
-      textDecoration: "underline",
+      fontSize: "fs-14",
+      ps: "1px",
+      mb: "5px",
+      mt: "0",
     },
   },
 };
 
-export default defaultLabelStyles;
+const defaultTextStyles = {
+  FormLabel: {
+    baseStyle: {
+      fontFamily: "Afterglow",
+      color: "secondary.500",
+      fontSize: "fs-14",
+      ps: "1px",
+      mb: "5px",
+      mt: "0",
+    },
+  },
+};
+
+export { defaultLabelStyles, defaultTextStyles };
