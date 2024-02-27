5f79e8ec47ebce62c52d9cd4bad79ee2eab55c96
Abdi714 <61796437+Abdi714@users.noreply.github.com>
2024-02-27 20:44:37 +0100

    Resolution des conflit sur le fichier App.tsx dédié pour les tests
    J'ai du supprimer le code se trouvant sur le fichier App.tsx
    Ce code avait enegdré des conflits avec le contenu du fichier App.tsx du code base.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { ChakraProvider, Box, extendTheme } from '@chakra-ui/react';
-import colors from './theme/colors';
+
 
 
 const theme = extendTheme({ colors });
@@ -10,22 +9,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <ChakraProvider theme={theme}>
-      <Box p={4} bg="primary.doggerBlue.0" color="white">
-        {/* Use the primary DoggerBlue color with 100% opacity */}
-        <h1>Hello, Chakra UI with Custom Colors!</h1>
-      </Box>
-
-      <Box p={4} bg="secondary.green.500" color="white" mt={4}>
-        {/* Use the secondary Green color with 100% opacity */}
-        <p>This is an example using the secondary Green color.</p>
-      </Box>
-
-      <Box p={4} bg="state.red.0" color="white" mt={4}>
-        {/* Use the state Red color with 100% opacity */}
-        <p>This is an example using the state Red color.</p>
-      </Box>
-    </ChakraProvider>
+   
   );
 }
 
